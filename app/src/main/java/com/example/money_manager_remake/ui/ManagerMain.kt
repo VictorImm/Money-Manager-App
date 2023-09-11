e4ca2bac0a63e724b7f8abcf69150499f2c61069
VictorImm <victorimmanuelss@gmail.com>
2023-09-12 00:11:31 +0700

    [UPDATE] enhancing live data visualization

--- app/src/main/java/com/example/money_manager_remake/ui/ManagerMain.kt
+++ app/src/main/java/com/example/money_manager_remake/ui/ManagerMain.kt
@@ -1,5 +1,9 @@
 package com.example.money_manager_remake.ui
 
+import android.animation.ObjectAnimator
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,8 +12,10 @@ import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,6 +29,7 @@ import com.example.money_manager_remake.data.viewmodel.IncomeViewModelFactory
 import com.example.money_manager_remake.data.viewmodel.OutcomeViewModel
 import com.example.money_manager_remake.data.viewmodel.OutcomeViewModelFactory
 import com.example.money_manager_remake.databinding.FragmentManagerMainBinding
+import com.example.money_manager_remake.method.NumberFormatter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ManagerMain : Fragment() {
@@ -41,6 +48,7 @@ class ManagerMain : Fragment() {
     private lateinit var tvExpenseLeft: TextView
     private lateinit var tvExpenseSpent: TextView
     private lateinit var tvExpenseTotal: TextView
+    private lateinit var progressBar: ProgressBar
     private lateinit var rvExpense: RecyclerView
 
     // animation
@@ -76,6 +84,7 @@ class ManagerMain : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -93,6 +102,9 @@ class ManagerMain : Fragment() {
         tvDate = binding.textDate
         tvDate.text = "${MainActivity.DATE} - ${MainActivity.MONTH} - ${MainActivity.YEAR}"
 
+        // bind progress bar
+        progressBar = binding.progressBar
+
         // bind expense and calculate
         tvExpenseTotal = binding.textBudget
         tvExpenseSpent = binding.textSpent
@@ -101,18 +113,34 @@ class ManagerMain : Fragment() {
         incomeViewModel.retrieveIncome(
             MainActivity.MONTH,
             MainActivity.YEAR
-        ).observe(this.viewLifecycleOwner) {
-            income = incomeViewModel.calculateIncome(it)
-            tvExpenseTotal.text = "Rp $income"
-        }
+        ).observe(this.viewLifecycleOwner) { listIncome ->
 
-        outcomeViewModel.retrieveTotalOutcome(
-            MainActivity.MONTH,
-            MainActivity.YEAR
-        ).observe(this.viewLifecycleOwner) {
-            outcome = outcomeViewModel.calculateOutcome(it)
-            tvExpenseSpent.text = "Rp $outcome"
-            tvExpenseLeft.text = "Rp ${income - outcome}"
+            outcomeViewModel.retrieveTotalOutcome(
+                MainActivity.MONTH,
+                MainActivity.YEAR
+            ).observe(this.viewLifecycleOwner) { listOutcome ->
+                // cache all data
+                income = incomeViewModel.calculateIncome(listIncome)
+                outcome = outcomeViewModel.calculateOutcome(listOutcome)
+
+                // show data
+                tvExpenseTotal.text = "Rp ${NumberFormatter.format(income)}"
+                tvExpenseSpent.text = "Rp ${NumberFormatter.format(outcome)}"
+                tvExpenseLeft.text = "Rp ${NumberFormatter.format(income - outcome)}"
+
+                // progress bar progress
+                ObjectAnimator.ofInt(progressBar, "progress", outcome)
+                    .setDuration(2000)
+                    .start()
+
+                // set progress bar max
+                progressBar.max = income
+
+                // change color if outcome is greater than income
+                if (outcome > income) {
+                    progressBar.progressTintList = ColorStateList.valueOf(Color.RED)
+                }
+            }
         }
 
         // bind recyclerview
