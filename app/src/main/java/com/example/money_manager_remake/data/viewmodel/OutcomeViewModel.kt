06e99632add8d39369ec142268e846e2fb9d594e
VictorImm <victorimmanuelss@gmail.com>
2023-09-15 01:08:58 +0700

    [UPDATE] use getDateDistinct

--- app/src/main/java/com/example/money_manager_remake/data/viewmodel/OutcomeViewModel.kt
+++ app/src/main/java/com/example/money_manager_remake/data/viewmodel/OutcomeViewModel.kt
@@ -3,8 +3,11 @@ package com.example.money_manager_remake.data.viewmodel
 import android.util.Log
 import androidx.lifecycle.*
 import com.example.money_manager_remake.MainActivity
+import com.example.money_manager_remake.data.DateDistinctResult
 import com.example.money_manager_remake.data.outcome.Outcome
 import com.example.money_manager_remake.data.outcome.OutcomeDao
+import com.jjoe64.graphview.series.DataPoint
+import kotlinx.coroutines.flow.toSet
 import kotlinx.coroutines.launch
 
 class OutcomeViewModel(private val outcomeDao: OutcomeDao): ViewModel() {
@@ -45,6 +48,11 @@ class OutcomeViewModel(private val outcomeDao: OutcomeDao): ViewModel() {
         return true
     }
 
+    // retrieve date
+    fun retrieveOutcomeDaily(m: Int, y: Int): LiveData<List<DateDistinctResult>> {
+        return outcomeDao.getDateDistinct(m, y).asLiveData()
+    }
+
     // retrieve item
     fun retrieveOutcome(d: Int, m: Int, y: Int): LiveData<List<Outcome>> {
         return outcomeDao.getDaily(d, m, y).asLiveData()
@@ -80,6 +88,23 @@ class OutcomeViewModel(private val outcomeDao: OutcomeDao): ViewModel() {
         outcome[8] = outcome[8]!! + calculateOutcome(listOutcome)
 
         return outcome
+    }
+    fun calculateOutcomeDaily(listOutcome: List<DateDistinctResult>): ArrayList<DataPoint> {
+        var prev = 0
+        var totalPrice = 0
+        val dataPointArray = ArrayList<DataPoint>()
+
+        for (o in listOutcome) {
+
+            if (o.d == prev) {
+                totalPrice += o.price
+            } else {
+                dataPointArray.add(DataPoint(prev.toDouble(), totalPrice.toDouble()))
+                totalPrice = o.price
+            }
+            prev = o.d
+        }
+        return dataPointArray
     }
 }
 
